4ae9b0382d4e0604b02ca80b43fd9322db973238
Adrien Gasquet <adrien.gsqt@gmail.com>
2024-04-11 16:26:07 +0200

    Implementing GSAP to improve anim-appear

---
+++ src/js/anim-landing.js
@@ -0,0 +1,33 @@
+const appearTimeline = gsap.timeline()
+
+appearTimeline.fromTo('.anim-appear',{
+    y:-15,
+    opacity:0
+},
+{
+    y:0,
+    opacity:1,
+    duration:0.3,
+    delay:0.30,
+    stagger:{
+        each:0.1,
+        from:0,
+    }
+}).fromTo('.anim-appear-header',{
+    y:-15,
+    opacity:0
+},
+{
+    y:0,
+    opacity:1,
+    duration:0.3,
+    stagger:0.2
+}).fromTo('.anim-appear-up',{
+    y:15,
+    opacity:0
+},
+{
+    y:0,
+    opacity:1,
+    duration:0.3,
+})
