b5bd032e9cd17e6190869ea55bb4c14de3ac59d2
AdrienGQT 🤸 <adrien.gsqt@gmail.com>
2025-02-25 21:22:26 +0100

    Gliss'N'Chill update

--- src/pages/design/gliss-n-chill.php
+++ src/pages/design/gliss-n-chill.php
@@ -4,28 +4,27 @@
     <div class="article-title">
         <h3 class="article-title__main">Gliss N Chill</h3>
         <p class="article-title__second">
-            Gliss N Chill is an interactive and immersive experiment developed by Ahmed Kabbej and me, and designed by
-            Nathan Limbos, Baptiste Gaudron, and Sofiane Hedaji.
+            Gliss'N'Chill is an interactive and immersive experiment developed by Ahmed Kabbej and me, with design
+            contributions from Nathan Limbos, Baptiste Gaudron, and Sofiane Hedaji.
         </p>
     </div>
     <ol class="article-sections-list">
 
         <li class="article-section">
-            <h5 class="article-section__title">Context</h5>
+            <h5 class="article-section__title">Project Background</h5>
             <p class="article-section__para">
-                Gliss n Chill was developed during a workshop called "Movement and Sounds."
-                The goal of this project was to create an immersive experience that reacts to movement and sound.
+                Gliss'N'Chill was developed during a workshop titled "Movement and Sounds." The project aimed to create
+                an immersive experience that responds to movement and sound.
             </p>
         </li>
 
         <li class="article-section">
-            <h5 class="article-section__title">Experience</h5>
+            <h5 class="article-section__title">Interactive Experience</h5>
             <p class="article-section__para">
-                Gliss n Chill places you in a serene forest environment surrounded by seven wisteria flowers.
-                Using body movement, you can interact with these flowers to create music.
-                Moving your right hand triggers different notes, with each flower representing a note in a Pentatonic
-                scale,
-                allowing you to compose unique melodies through movement.
+                Gliss'N'Chill transports you to a serene forest environment surrounded by seven wisteria flowers. By
+                moving your body, you can interact with these flowers to create music. Moving your right hand triggers
+                different notes, with each flower representing a note in a Pentatonic scale, allowing you to compose
+                unique melodies through movement.
             </p>
         </li>
 
@@ -35,24 +34,52 @@
             </div>
             <video autoplay muted loop class="article-media__video"
                 src="\client-data\images\pages-content\gliss-n-chill\demo.webm"
-                alt="Video of Gobelins Timeline"></video>
+                alt="Video of Gliss'N'Chill Demonstration"></video>
         </div>
 
         <li class="article-section">
-            <h5 class="article-section__title">Technologies Used</h5>
+            <h5 class="article-section__title">Movement Tracking</h5>
             <p class="article-section__para">
-                The project is built using Mediapipe for real-time hand tracking, Matter.js for realistic physics
-                simulation,
-                and Vite for fast development. The structure and styling are handled with JavaScript, CSS, and HTML.
+                To create this project, we needed to track the user's hand position in real-time. We achieved this using
+                the user's webcam video feed combined with the <a
+                    href="https://ai.google.dev/edge/mediapipe/solutions/guide?hl=en" target="_blank">Google MediaPipe
+                    API</a>. This part of the code captures the position of the user's right hand in every frame.
             </p>
         </li>
 
         <li class="article-section">
-            <h5 class="article-section__title">Development Notes</h5>
+            <h5 class="article-section__title">Physics Simulation</h5>
             <p class="article-section__para">
-                This project is designed as a real-time interactive experience, blending movement and sound to create
-                a calm and engaging atmosphere. By combining hand tracking and physics simulation,
-                it delivers an immersive musical experience.
+                Another crucial aspect of the development was the physics simulation of the flowers. We used the <a
+                    href="https://brm.io/matter-js/" target="_blank">Matter.js library</a> to create and simulate
+                complex objects with constraints and realistic physical behavior. This part of the code retrieves the
+                user's hand position from the tracker, moves the circle, and calculates interactions with the flowers,
+                considering speed and direction to create a sensation of gently touching the flowers.
+            </p>
+        </li>
+
+        <li class="article-section">
+            <h5 class="article-section__title">Note Generation</h5>
+            <p class="article-section__para">
+                We then needed to detect collisions between the circle and the flowers. When a flower is touched, a
+                corresponding note is emitted, allowing the user to create melodies.
+            </p>
+        </li>
+
+        <li class="article-section">
+            <h5 class="article-section__title">Sound Environment</h5>
+            <p class="article-section__para">
+                To fully immerse the user, we created a complex sound environment. We mixed forest sounds, including
+                wind and nature noises. Additionally, we composed a calm and relaxing musical background in the same
+                scale as the flower notes to complement the user's melody.
+            </p>
+        </li>
+
+        <li class="article-section">
+            <h5 class="article-section__title">Try it now</h5>
+            <p class="article-section__para">
+                Our project is available online ! You can try it on desktop right now by clicking <a
+                    href="https://glisse-n-chill.vercel.app/" target="_blank">here</a>
             </p>
         </li>
 
